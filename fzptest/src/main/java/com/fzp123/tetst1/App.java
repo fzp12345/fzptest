16b78bda42870704792bb673f7c45be40aa3a8bb
fzp12345 <1173554338@qq.com>
2018-04-15 21:25:27 +0800

    哈哈哈

---
+++ fzptest/src/main/java/com/fzp123/tetst1/App.java
@@ -0,0 +1,13 @@
+package com.fzp123.tetst1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ fzptest/src/test/java/com/fzp123/tetst1/AppTest.java
@@ -0,0 +1,20 @@
+package com.fzp123.tetst1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
